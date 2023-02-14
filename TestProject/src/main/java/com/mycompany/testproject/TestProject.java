54a69a76e12c415a84c034e0fcab22397bdd5775
remon <blackshadowsoftwareltd@gmail.com>
2023-02-14 21:00:31 +0600

    first test project

---
+++ TestProject/src/main/java/com/mycompany/testproject/TestProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testproject;
+
+/**
+ *
+ * @author remon
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
