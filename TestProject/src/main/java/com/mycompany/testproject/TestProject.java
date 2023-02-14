695749ce6cc870cb58de128c1b56279bbc163413
remon <blackshadowsoftwareltd@gmail.com>
2023-02-14 21:11:43 +0600

    run project from vs code

--- TestProject/src/main/java/com/mycompany/testproject/TestProject.java
+++ TestProject/src/main/java/com/mycompany/testproject/TestProject.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.testproject;
 
-/**
- *
- * @author remon
- */
 public class TestProject {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Start");
     }
 }
