7ad6397c70e767f40b72f96bdf331657f80d3909
remon <blackshadowsoftwareltd@gmail.com>
2023-03-02 08:30:53 +0600

    basic test init

---
+++ Basic_practice/src/main/java/com/mycompany/testproject/TestProject.java
@@ -0,0 +1,8 @@
+package com.mycompany.testproject;
+
+public class TestProject {
+    public static void main(String[] args) {
+        PrimeNumber p = new PrimeNumber();
+        p.primeNumber();
+    }
+}
