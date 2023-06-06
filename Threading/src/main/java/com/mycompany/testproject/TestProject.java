27857e1e55c6a81cc0b9b1b814d4959b42395395
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-06-06 18:34:56 +0600

    cv maker project init

--- Threading/src/main/java/com/mycompany/testproject/TestProject.java
+++ Threading/src/main/java/com/mycompany/testproject/TestProject.java
@@ -2,37 +2,44 @@ package com.mycompany.testproject;
 
 public class TestProject {
     public static void main(String[] args) {
-        System.out.println("Start");
-        TestProject t = new TestProject();
-        t.assign();
-        t.print();
-        Init init = new Init();
-        init.setStatic();
-        init.print();
-    }
 
-    void assign() {
-        Test test = new Test();
-        test.name = "test";
-        Test.s = "static";
-        System.out.println(test.hashCode());
-    }
+        int x = 1;
 
-    void print() {
-        Test test = new Test();
-        System.out.println(test.name);
-        System.out.println(Test.s);
-        System.out.println(test.hashCode());
+        RunnableThreads runnable1 = new RunnableThreads("Thread 1");
+        RunnableThreads runnable2 = new RunnableThreads("Thread 2");
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main thread exiting.");
     }
 
 }
 
-class Init {
-    void setStatic() {
-        Test.s = "static updated";
+class RunnableThreads implements Runnable {
+    private final String message;
+
+    public RunnableThreads(String message) {
+        this.message = message;
     }
 
-    void print() {
-        System.out.println(Test.s);
+    @Override
+    synchronized public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(message + " " + i);
+        }
     }
+
 }
+
+// 183015128 Rakibul Islam
